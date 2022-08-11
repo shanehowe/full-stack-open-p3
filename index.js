7d1dfaed940560f662781e5e6e0481987899910d
Shane Howe <shanehowe100@hotmail.com>
2022-08-11 16:48:07 -0700

    front end build added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token("data", (request) => {
     return request.method === "POST" ? JSON.stringify(request.body) : " ";
