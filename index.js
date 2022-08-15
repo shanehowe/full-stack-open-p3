f642c33fcbd080583f95db835410578d8bb3f164
Shane Howe <shanehowe100@hotmail.com>
2022-08-15 11:47:32 -0700

    uibuild

--- index.js
+++ index.js
@@ -85,15 +85,11 @@ app.post('/api/persons', (request, response, next) => {
 })
 
 app.put("/api/persons/:id", (request, response, next) => {
-    const id = request.params.id
-    const body = request.body
+    const { name, number } = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-
-    Person.findByIdAndUpdate(id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, 
+        { name, number }, 
+        { new: true, runValidators: true, context: "query" })
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
--- models/person.js
+++ models/person.js
@@ -3,9 +3,18 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 console.log("Connecting to ", url)
 
-mongoose.connect(url)
+mongoose
+    .connect(url)
     .then(result => console.log("Connected to MongoDB"))
     .catch(error => console.log("Error connecting: ", error.message))
+
+const validateNumber = (number) => {
+    if ((number[2] === "-" || number[3] === "-") && number.length < 9)
+    {
+        return false
+    }
+    return true
+}
 
 const PersonSchema = new mongoose.Schema(
     {
@@ -16,6 +25,16 @@ const PersonSchema = new mongoose.Schema(
         },
         number: {
             type: String,
+            validate: [
+                {
+                    validator: validateNumber,
+                    message: "Minimum number length must be greater than or equal to 9",
+                },
+                {
+                    validator: (number) => /\d{2,3}-\d{6}/.test(number),
+                    message: "Invalid phone number"
+                }
+            ],
             required: true
         }
     }
